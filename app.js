0ab07e65ece1091f4c9206e47e9eaffd293fddee
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-03-14 16:26:16 -0300

    fix routing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 require('express-async-errors')
 
 const cors = require('cors');
@@ -7,8 +8,12 @@ const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
 
 
+
 app.use(cors());
 app.use(express.static('dist'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
