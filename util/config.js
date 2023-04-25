16d685b2cdb62bc8adad37e6b91675a4e71d0c00
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-04-25 11:10:59 -0300

    cross env (adaptar se é dev ou deploy)

--- util/config.js
+++ util/config.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.NODE_ENV === 'development' ? process.env.PORT || 8080: process.env.PORT || 3001,
   SECRET: process.env.SECRET
 }
+
+
