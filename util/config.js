c2152a233dd7bb2249248c64ff3fdd46ccf523b0
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-04-25 00:56:03 -0300

    fly.io(porta 8080 quebrando local)

--- util/config.js
+++ util/config.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 8080,
   SECRET: process.env.SECRET
 }
