0a85e1f811380e9235de17ff9368c0c1612643ce
kevin-weitgenant <kevin.weitgenant@gmail.com>
2023-08-11 17:49:39 -0300

    .env

--- util/config.js
+++ util/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
   PORT: process.env.NODE_ENV === 'development' ? process.env.PORT || 8080: process.env.PORT || 3001,
-  SECRET: process.env.SECRET
+  // SECRET: process.env.SECRET
 }
 
 
