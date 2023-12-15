2588603e7162a38df17e4e18033fec28016afaa7
Gabriel <falivith@gmail.com>
2023-12-15 10:44:55 -0300

    localdb

--- util/config.js
+++ util/config.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_URL: process.env.DATABASE_URL_LOCAL, // Aqui
   PORT: process.env.NODE_ENV === 'development' ? process.env.PORT || 8080: process.env.PORT || 3001,
   // SECRET: process.env.SECRET
 }
-
-
--- util/db.js
+++ util/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const { DATABASE_URL } = require('./config')
 
-const sequelize = new Sequelize(DATABASE_URL, { dialect: 'postgres', logging: false });
+const sequelize = new Sequelize( DATABASE_URL, { dialect: 'postgres', logging: false });
 
 const connectToDatabase = async () => {
   try {
