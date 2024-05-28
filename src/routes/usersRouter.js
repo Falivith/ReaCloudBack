ecadd49072f26c5356b30d3e573219265f47ec5c
Maria Eduarda N. Carmona <eduardacarmona@gmail.com>
2024-05-28 00:16:00 -0300

    :adhesive_bandage: Ajuste no userRouter para o fetchData do front

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -15,13 +15,13 @@ usersRouter.get('/uploadPhoto', async (req, res) => {
 
   const user = await User.findByPk(decodedToken.id);
 
-    if (user.profilePicture) {
-      res.set('Content-Disposition', 'inline');
-      res.json({data: user.profilePicture})
-    } else {
-      console.log('Foto não encontrada.');
-      return res.status(200);
-    } 
+  if (user.profilePicture) {
+    res.set('Content-Disposition', 'inline');
+    res.json({ data: user.profilePicture })
+  } else {
+    console.log('Foto não encontrada.');
+    return res.status(200);
+  }
 })
 
 // Consulta de todos os usuários
@@ -38,32 +38,32 @@ usersRouter.post('/', async (req, res) => {
   }
 
   const saltRounds = 10
-  
+
   const existingUser = await User.findOne({ where: { email: req.body.email } });
   if (existingUser) {
     return res.status(400).json({ error: 'User already exists' });
   }
-  
-  const passwordHashed = await bcrypt.hash(req.body.password,saltRounds)
-  const user = await User.create({...req.body, password:passwordHashed })
+
+  const passwordHashed = await bcrypt.hash(req.body.password, saltRounds)
+  const user = await User.create({ ...req.body, password: passwordHashed })
   res.status(201).json(user)
 })
 
 // Alteração de senha
 usersRouter.put('/dados', async (req, res) => {
   const decodedToken = await util.checkToken(req)
-  const {password,newPassword} = req.body
+  const { password, newPassword } = req.body
   const user = await User.findByPk(decodedToken.id);
   const passwordCorrect = user === null
-  ? false
-  : await bcrypt.compare(password, user.password)
+    ? false
+    : await bcrypt.compare(password, user.password)
 
   if (!(user && passwordCorrect)) {
-      return res.status(401).json({
-        error: 'invalid username or password'
-      })
-    }
-  else{
+    return res.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+  else {
     const hashedPassword = await bcrypt.hash(newPassword, 10);
     user.password = hashedPassword;
     await user.save();
@@ -73,16 +73,20 @@ usersRouter.put('/dados', async (req, res) => {
 
 // Consulta de dados da conta
 usersRouter.get('/:email', async (req, res) => {
-  const decodedToken = await util.checkToken(req)
-    
-  const user = await User.findByPk(decodedToken.id);
-  if (user) {
-    res.status(200).json(user)
-  } else {
-    console.log('Usuário não encontrado.');
-    return res.status(404).json({ error: 'Usuário não encontrado.' });
-  } 
-})
+  const email = req.params.email;
+  try {
+    const user = await User.findOne({ where: { email } }); // Find user by email
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      console.log('Usuário não encontrado.');
+      return res.status(404).json({ error: 'Usuário não encontrado.' });
+    }
+  } catch (error) {
+    console.error('Erro ao procurar usuário:', error);
+    return res.status(500).json({ error: 'Erro ao procurar usuário.' });
+  }
+});
 
 // Consulta nome e foto do usuário por ID
 usersRouter.get('/:id/info', async (req, res) => {
@@ -102,7 +106,7 @@ usersRouter.get('/:id/info', async (req, res) => {
     res.status(500).json({ error: 'Erro interno do servidor' });
   }
 });
-  
+
 // Alteração de dados da conta 
 usersRouter.put('/:email', async (req, res) => {
   try {
