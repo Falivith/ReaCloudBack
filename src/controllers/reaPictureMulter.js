aaa64611afb04a95d66b4f78175c09bd9042b1ff
Ulian Gabriel <falivith@gmail.com>
2024-05-30 23:27:56 -0300

    Arquivo temporário de imagens

--- src/controllers/reaPictureMulter.js
+++ src/controllers/reaPictureMulter.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 module.exports = (multer({
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-            cb (null, './uploads/reas')
+            cb (null, './uploads')
         },
         filename: (req, file, cb) => {
             cb (null, Date.now().toString() + "_" + file.originalname)
